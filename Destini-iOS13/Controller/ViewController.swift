c41b60babff72ddf0d18f67895bdbd03bc522aae
marufkhan09 <maruf1609@gmail.com>
2023-10-02 01:32:34 +0600

    initial changes done

---
+++ Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
+    override func viewDidLoad() {
+        storyLabel.text = storyBrain.example[storyBrain.destination].title
+        choice1Button.setTitle(storyBrain.example[storyBrain.destination].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.example[storyBrain.destination].choice2, for: .normal)
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func choiceTapped(_ sender: UIButton) {
+        updateUI(selectedChoice: storyBrain.destination,sender: sender)
+    }
+    
+    
+    @objc func updateUI(selectedChoice: Int,sender:UIButton){
+        
+        storyLabel.text = storyBrain.nextStory(selectedDest: selectedChoice).title
+        choice1Button.setTitle(storyBrain.nextStory(selectedDest: selectedChoice).choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.nextStory(selectedDest: selectedChoice).choice2, for: .normal)
+        print(sender.currentTitle!)
+//        if(sender.currentTitle){
+//            
+//        }else{
+//            
+//        }
+        
+    }
+    
+}
+
